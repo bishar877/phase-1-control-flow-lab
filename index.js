b3e62a10a70ef2afc6895cec3fd419414edb11b4
bishar <bishar.dahir@student.moringaschool.com>
2023-12-16 14:51:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance){
+  if (rideDistance <= 400) {
+    return 'This one is on me!';
+} else if (rideDistance > 2000 && rideDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+} else if (rideDistance > 2500) {
+    return 'No can do.';
+} else {
+    return 'Pay up.';}
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+        return 'Bye.';}
   // Write your code here!
 }
